9b40cb4a5b1d37e473848f239431a3549ea641a0
InhaLSH <ss2rms@gmail.com>
2022-03-25 14:59:18 +0900

    4주차 파일 업로드

---
+++ DS Week4-1/DS Week4-1.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <string>
+
+class ArrayQueue {
+public :
+	ArrayQueue(int Cap) {
+		this->Capacity = Cap + 1;
+		Que = new int[Capacity];
+		Front = Rear = 0;
+		for (int i = 0; i < Capacity; i++) {
+			Que[i] = 0;
+		}
+	}
+	void size() {
+		std::cout << (Rear - Front + Capacity)%Capacity << std::endl;
+	}
+	void isEmpty() {
+		std::cout << std::boolalpha << (Front == Rear) << std::endl;
+	}
+	void front() {
+		if (Front == Rear) {
+			std::cout << "Empty" << std::endl;
+		}
+		else {
+			std::cout << Que[(Front + 1) % Capacity] << std::endl;
+		}
+	}
+	void rear() {
+		if (Front == Rear) {
+			std::cout << "Empty" << std::endl;
+		}
+		else {
+			std::cout << Que[Rear] << std::endl;
+		}
+	}
+	void enqueue(int Value) {
+		if (Front == (Rear + 1)%Capacity) {
+			std::cout << "Full" << std::endl;
+		}
+		else {
+			Rear = (Rear + 1)%Capacity;
+			Que[Rear] = Value;
+		}
+	}
+	void dequeue() {
+		if (Front == Rear) {
+			std::cout << "Empty" << std::endl;
+		}
+		else {
+			Front = (Front + 1)%Capacity;
+			Que[Front] = 0;
+		}
+	}
+private :
+	int* Que;
+	int Rear;
+	int Front;
+	int Capacity;
+};
+
+int main() {
+	int Counter;
+	int SizeOfQueue;
+	std::cin >> SizeOfQueue >> Counter;
+
+	ArrayQueue Queue1(SizeOfQueue);
+	for (int i = 0; i < Counter; i++) {
+		std::string Command;
+		std::cin >> Command;
+
+		if (Command == "size") {
+			Queue1.size();
+		}
+		else if (Command == "isEmpty") {
+			Queue1.isEmpty();
+		}
+		else if (Command == "front") {
+			Queue1.front();
+		}
+		else if (Command == "rear") {
+			Queue1.rear();
+		}
+		else if (Command == "enqueue") {
+			int EnqueueValue;
+			std::cin >> EnqueueValue;
+			Queue1.enqueue(EnqueueValue);
+		}
+		else if (Command == "dequeue") {
+			Queue1.dequeue();
+		}
+		else {
+			std::cout << "Invalid Input" << std::endl;
+		}
+	}
+}
---
+++ DS Week4-2/DS Week4-2.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <string>
+
+class Node {
+public :
+	int Data;
+	Node* Next;
+
+	Node(int Value) {
+		this->Data = Value;
+		this->Next = NULL;
+	}
+};
+
+class LinkedList {
+public :
+	Node* Front;
+	Node* Rear;
+
+	LinkedList() {
+		this->Front = NULL;
+		this->Rear = NULL;
+	}
+	void addEnd(int Value) {
+		Node* NewNode = new Node(Value);
+		if (Front == NULL) {
+			Front = Rear = NewNode;
+		}
+		else {
+			Rear->Next = NewNode;
+			Rear = NewNode;
+		}
+	}
+	void removeFront() {
+		if (Front != NULL) {
+			Node* TempNode = Front;
+			Front = Front->Next;
+			delete TempNode;
+		}
+	}
+};
+
+class ListQueue {
+public :
+	ListQueue() {
+		this->Que = new LinkedList();
+		this->SizeOfQueue = 0;
+	}
+	void size() {
+		std::cout << SizeOfQueue << std::endl;
+	}
+	void isEmpty() {
+		std::cout << std::boolalpha << (this->Que->Front == NULL) << std::endl;
+	}
+	void front() {
+		if (this->Que->Front == NULL) {
+			std::cout << "Empty" << std::endl;
+		}
+		else {
+			std::cout << this->Que->Front->Data << std::endl;
+		}
+	}
+	void rear() {
+		if (this->Que->Front == NULL) {
+			std::cout << "Empty" << std::endl;
+		}
+		else {
+			std::cout << this->Que->Rear->Data << std::endl;
+		}
+	}
+	void enqueue(int Value) {
+		this->Que->addEnd(Value);
+		SizeOfQueue++;
+	}
+	void dequeue() {
+		if (this->Que->Front == NULL) {
+			std::cout << "Empty" << std::endl;
+		}
+		else {
+			this->Que->removeFront();
+			SizeOfQueue--;
+		}
+	}
+private :
+	LinkedList* Que;
+	int SizeOfQueue;
+};
+
+int main() {
+	ListQueue Queue2;
+
+	int Counter;
+	std::cin >> Counter;
+	for (int i = 0; i < Counter; i++) {
+		std::string Command;
+		std::cin >> Command;
+
+		if (Command == "size") {
+			Queue2.size();
+		}
+		else if (Command == "isEmpty") {
+			Queue2.isEmpty();
+		}
+		else if (Command == "front") {
+			Queue2.front();
+		}
+		else if (Command == "rear") {
+			Queue2.rear();
+		}
+		else if (Command == "enqueue") {
+			int EnqueueValue;
+			std::cin >> EnqueueValue;
+			Queue2.enqueue(EnqueueValue);
+		}
+		else if (Command == "dequeue") {
+			Queue2.dequeue();
+		}
+		else {
+			std::cout << "Invalid Input" << std::endl;
+		}
+	}
+}
